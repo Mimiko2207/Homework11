223faeef3b56aacea55afb47ce10f7788bcef5d9
Mimiko2207 <loparev.m@mail.ru>
2025-08-06 19:01:54 +0700

    ДЗ-11 Готово

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,11 @@ public class Main {
     // ЗАДАЧА 1
     public static void main(String[] args) {
         System.out.println(checkLeapYear(2024));
+        System.out.println(calculateDeviceVersion(2020, 1));
+        int days = calculateDelivery(95);
+        System.out.println("Потребуется дней: " + days);
+
+
     }
 
     public static String checkLeapYear(int i) {
@@ -11,32 +16,43 @@ public class Main {
             return i + " год високосный ";
         } else {
             return i + " год не високосный ";
-
         }
     }
-
     // ЗАДАЧА 2
-    public static void main1(String[] args) {
-        System.out.println(calculateDeviceVersion(2010));
-    }
 
-    public static String calculateDeviceVersion(int i, int i1) {
-        if (i1 == 1) {
-            if (i <= 2015) {
+
+    public static String calculateDeviceVersion(int deviceType, int manufactureYear) {
+        if (manufactureYear == 1) {
+            if (deviceType <= 2015) {
                 return "Установите облегченную версию приложения для Android по ссылке";
             } else {
                 return "Установите версию приложения для Android по ссылке";
             }
         } else {
-            if (i <= 2015) {
+            if (deviceType <= 2015) {
                 return "Установите облегченную версию приложения для iOS по ссылке";
             } else {
                 return "Установите версию приложения для iOS по ссылке";
             }
-
         }
     }
-}
+
+    // Задача 3
+        public static int calculateDelivery(int deliveryDistance) {
+            int term = 1;
+            if (deliveryDistance <= 20) {
+                return term;
+            } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+                term = term + 1;
+                return term;
+            } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+                term = term + 1 + 1;
+                return term;
+            } else {
+                return -1;
+            }
+        }
+    }
 
 
 
